c35ab5681248756a61650759748f19e4d0d5f1eb
vsmr8009 <vsmr8009@gmail.com>
2021-05-19 19:32:05 +0530

    Employee management practise

---
+++ Employee Management/Angular/employee-management/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,45 @@
+import { Employee } from './../employee';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit{
+
+  employees: Employee[];
+
+  constructor(private employeeService: EmployeeService, private router: Router) {}
+
+  ngOnInit(): void{
+    this.getEmployees();
+  }
+
+  getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data =>{
+      this.employees = data;
+    })
+  }
+
+  updateEmployee(id: number){
+    this.router.navigate(["update-employee", id]);
+  }
+
+  viewEmployee(id: number){
+    this.router.navigate(["employee-details", id]);
+  }
+
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id).subscribe(data =>{
+      console.log(data);
+      this.getEmployees();
+    })
+  }
+
+}
+
+ 
+
